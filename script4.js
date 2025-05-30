c02d31802da3c0801c3ce0495cb707316d4584c5
Salomee1 <salomechkhetiani08@gmail.com>
2025-05-30 22:03:48 +0400

    create js4

---
+++ script4.js
@@ -0,0 +1,16 @@
+let colors = ['Red', 'Green', 'Blue']; 
+
+if (colors.length > 0) {
+  
+  let firstColor = colors[0];
+
+  1
+  let lastColor = colors[colors.length - 1];
+
+  
+  alert("The first color is: " + firstColor);
+  alert("The last color is: " + lastColor);
+} else {
+  
+  alert("The color array is empty.");
+}
