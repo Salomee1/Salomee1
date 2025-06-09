15879f324ae8d41ab6e3e22013b731dcdb57bf45
Salomee1 <salomechkhetiani08@gmail.com>
2025-06-09 21:51:01 +0400

    update

---
+++ script17.js
@@ -0,0 +1,6 @@
+function haveSameLength(word1, word2) {
+  return word1.length === word2.length;
+}
+console.log(haveSameLength("hello", "world"));   
+console.log(haveSameLength("apple", "banana"));  
+console.log(haveSameLength("hi", "no"));         
