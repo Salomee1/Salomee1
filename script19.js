614810b0860649e4d158b448524fe31479a52062
Salomee1 <salomechkhetiani08@gmail.com>
2025-06-11 23:08:41 +0400

    update

---
+++ script19.js
@@ -0,0 +1,14 @@
+function getDivisors(number) {
+  const divisors = [];
+
+  for (let i = 1; i <= number; i++) {
+    if (number % i === 0) {
+      divisors.push(i);
+    }
+  }
+
+  return divisors;
+}
+
+
+console.log(getDivisors(12)); // [1, 2, 3, 4, 6, 12]
